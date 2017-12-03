f37a0a68570a71067806163c87d3bc6ef4c2d7c1
Manohar Kurapati <kvmanohar@gmail.com>
2017-12-03 19:28:24 +0000

    Completed AuthVC and LoginVC UI

---
+++ breakpoint/View/InsetTextField.swift
@@ -0,0 +1,15 @@
+//
+//  InsetTextField.swift
+//  breakpoint
+//
+//  Created by Manohar Kurapati on 03/12/2017.
+//  Copyright © 2017 Manosoft. All rights reserved.
+//
+
+import UIKit
+
+class InsetTextField: UITextField {
+
+
+
+}
