ea6e5ea67692fb07cd40621a07027777d20b1498
Manohar Kurapati <kvmanohar@gmail.com>
2017-12-03 16:00:36 +0000

    Created DataService

---
+++ breakpoint/Services/DataService.swift
@@ -0,0 +1,40 @@
+//
+//  DataService.swift
+//  breakpoint
+//
+//  Created by Manohar Kurapati on 03/12/2017.
+//  Copyright © 2017 Manosoft. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+let DB_BASE = Database.database().reference()
+
+class DataService {
+    static let instance = DataService()
+    
+    private var _REF_BASE = DB_BASE
+    private var _REF_USERES = DB_BASE.child("users")
+    private var _REF_GROUPS = DB_BASE.child("groups")
+    private var _REF_FEED = DB_BASE.child("feed")
+    
+    var REF_BASE: DatabaseReference {
+        return _REF_BASE
+    }
+    var REF_USERS: DatabaseReference {
+        return _REF_USERES
+    }
+    var REF_GROUPS: DatabaseReference {
+        return _REF_GROUPS
+    }
+    var REF_FEED: DatabaseReference {
+        return _REF_FEED
+    }
+    
+    func createDBUser(uid: String, userData: Dictionary<String, Any>){
+        REF_USERS.child(uid).updateChildValues(userData)
+    }
+    
+    
+}
