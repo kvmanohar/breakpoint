23e47bd79dfac7339064b53460e5493356cdf510
Manohar Kurapati <kvmanohar@gmail.com>
2017-12-06 21:47:18 +0000

    Completed Posting Feed to Firebase service

---
+++ breakpoint/Controller/CreatePostVC.swift
@@ -0,0 +1,60 @@
+//
+//  CreatePostVC.swift
+//  breakpoint
+//
+//  Created by Manohar Kurapati on 06/12/2017.
+//  Copyright © 2017 Manosoft. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class CreatePostVC: UIViewController {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var sendBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        textView.delegate = self
+    
+    }
+
+    
+    @IBAction func sendBtnPressed(_ sender: Any) {
+        
+        if textView.text != nil && textView.text != "Say something here..." {
+            sendBtn.isEnabled = false
+            
+            guard let uid = Auth.auth().currentUser?.uid else { return }
+            
+            DataService.instance.uploadPost(withMessage: textView.text, forUID: uid, withGroupKey: nil,
+                                            sendComplete: { (isComplete) in
+                if isComplete {
+                    self.sendBtn.isEnabled = true
+                    self.dismiss(animated: true, completion: nil)
+                } else {
+                    self.sendBtn.isEnabled = true
+                    print("Error while posting message.")
+                }
+            })
+        }
+        
+    }//sendBtnPressed
+    
+    @IBAction func closeBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
+
+extension CreatePostVC: UITextViewDelegate {
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textView.text = ""
+    }
+    
+    
+}
