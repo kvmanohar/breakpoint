252964eed5a10316322bdc9065e815c6f9e5b629
Manohar Kurapati <kvmanohar@gmail.com>
2017-12-11 19:32:13 +0000

    Created Group Collection in Firebase

--- breakpoint/Controller/CreateGroupVC.swift
+++ breakpoint/Controller/CreateGroupVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class CreateGroupVC: UIViewController {
 
@@ -50,7 +51,22 @@ class CreateGroupVC: UIViewController {
     
     @IBAction func doneBtnPressed(_ sender: Any) {
         
-    }
+        if titleTextField.text != "" && descriptionTextField.text != "" {
+            DataService.instance.getIds(forUsernames: chosenUserArray, handler: { (idsArray) in
+                var userIds = idsArray
+                userIds.append((Auth.auth().currentUser?.uid)!)
+                
+                DataService.instance.ceateGroup(withTitle: self.titleTextField.text!, andDescription: self.descriptionTextField.text!, forUserIds: userIds, handler: { (groupCreated) in
+                    if groupCreated {
+                        self.dismiss(animated: true, completion: nil)
+                    } else {
+                        print("Group could not be created. Plesae try again later!")
+                    }
+                })
+            })
+        }
+        
+    }//doneBtnPressed
     
     @IBAction func closeBtnPressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
